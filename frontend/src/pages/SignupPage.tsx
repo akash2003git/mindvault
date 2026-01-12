227aac94cb2cfab401e5f8372a6bd91e648ddee0
akash2003git <akash.tayade.dev@gmail.com>
2026-01-12 13:03:29 +0530

    Login and Signup page

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router"
+
 const SignupPage = () => {
   return (
-    <div>Singup Page</div>
+    <div className="flex items-center justify-center bg-white min-h-screen">
+      <div className="shadow-xl rounded-xl p-8 flex flex-col gap-5">
+        <h1 className="text-center text-3xl p-5">Create an account</h1>
+        <form className="flex flex-col gap-5">
+          <input id="identifier" type="string" placeholder="Username" className="px-4 py-2 rounded-3xl border border-gray-400" />
+          <input id="identifier" type="email" placeholder="Email" className="px-4 py-2 rounded-3xl border border-gray-400" />
+          <input id="password" type="password" placeholder="Password" className="px-4 py-2 rounded-3xl border border-gray-400" />
+          <button type="submit" className="font-bold px-4 py-2 bg-black text-center text-white rounded-3xl cursor-pointer hover:bg-gray-800">Sign Up</button>
+        </form>
+        <span className="text-center">Already have an account? <Link to="/login" className="hover:underline text-blue-500">Login</Link></span>
+      </div>
+    </div>
   )
 }
 
